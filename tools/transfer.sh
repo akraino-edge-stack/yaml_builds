b06788b0199912b764d7d9f0cec9d44a9dd01541
Naga Sugguna <ns156u@att.com>
2018-10-16 14:36:28 -0400

    adding node
    hpgen10 updated as a 3 node cluster
    1prom-gens.sh fixed not cleanup certificates everytime
    transfer.sh created just to transfer files genesis node and update setting for newly added nodes.
    
    Change-Id: I3c47848c858c1e108de60e3b82e815a9d0a3eb32

---
+++ tools/transfer.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.        #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License.                   #
+#                                                                            #
+# You may obtain a copy of the License at                                    #
+#       http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT  #
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.           #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+##############################################################################
+
+set -x
+TIMESTAMP=$(date +"%Y%m%d%H%M")
+echo "Logging to /var/log/yaml_builds/transfer_$TIMESTAMP.log"
+exec > /var/log/yaml_builds/transfer_$TIMESTAMP.log
+exec 2>&1
+
+source $(dirname $0)/setenv.sh
+
+if [ -z "$1" ]
+then
+  echo "Please pass site name as command line argument"
+  exit -2
+else
+  SITE=${SITE:-$1}
+  echo "SITE=$SITE"
+fi
+
+if [ -z "$YAML_BUILDS" ]
+then
+  echo "Please set YAML_BUILDS"
+  exit -3
+fi
+
+
+source $(dirname $0)/env_$SITE.sh
+
+cd $YAML_BUILDS
+
+scp $YAML_BUILDS/tars/promenade-bundle-$SITE.tar $GENESIS_HOST:/tmp/
+ssh $GENESIS_HOST << EOF
+  # TODO avoid following hard coding$
+  cp /tmp/promenade-bundle-$SITE.tar /root/akraino/
+  cd /root/akraino/
+  tar -xmf promenade-bundle-$SITE.tar
+EOF
+# Update BIOS Setting
+python $YAML_BUILDS/scripts/update_bios_settings.py $SITE.yaml

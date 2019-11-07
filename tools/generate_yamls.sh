0f183cf32f7333f68a54deb5c174c85caefdddfb
Georg Kunz <georg.kunz@ericsson.com>
2019-11-07 13:12:40 +0000

    Correctly delete seaworthy certificates
    The certificates shipped with the treasuremap repo were not deleted
    correctly before generating new ones for our site, resulting
    in broken certificates and failing cluster deployment.
    
    Change-Id: I3f72fded547505840025edc75ad16a525a6501d6

--- tools/generate_yamls.sh
+++ tools/generate_yamls.sh
@@ -72,7 +72,7 @@ mkdir -p $AIRSHIP_TREASUREMAP/site/$SITE
 echo "#   Copying seaworth config files"
 cp -r $AIRSHIP_TREASUREMAP/site/seaworthy/* $AIRSHIP_TREASUREMAP/site/$SITE
 echo "#   Removing seaworthy example certificate files"
-rm -f $AIRSHIP_TREASUREMAP/site/$SITE/secrets/certificates/
+rm -rf $AIRSHIP_TREASUREMAP/site/$SITE/secrets/certificates/
 echo "#   Overlaying [$SITE] specific config files"
 cp -r $YAML_BUILDS/site/$SITE/* $AIRSHIP_TREASUREMAP/site/$SITE
 
